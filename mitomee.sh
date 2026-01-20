0e7a3ed5447b77b30627ab3d2a743bab79e9d98a
npbhavya <npbhavya13@gmail.com>
2026-01-20 12:57:52 +0100

    changes from cluster

--- mitomee.sh
+++ mitomee.sh
@@ -13,3 +13,9 @@
 mitomee run --input test-files/metagenomes --extn fastq.gz --sequencing paired \
      --host_seq test-files/am-dh4.fasta \
      --conda-frontend mamba --output output
+
+#build tree with host seq, assembled mitogenomes. 
+#Also add other reference mitogenomes to  output/REPORTS/mitogenomes. No need to add the reference sequence, it will be included
+#Then run the subcommand to build tree 
+
+mitomee tree --input test-files/mitogenomes --extn fasta --host_seq test-files/am-dh4.fasta --output output -k all
